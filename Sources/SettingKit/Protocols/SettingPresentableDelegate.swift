6178464ea3be4508c83130557d3ef76292ea52bd
JeongTaek Han <smart8612@gmail.com>
2023-05-15 15:34:40 +0900

    feat: add protocols

---
+++ Sources/SettingKit/Protocols/SettingPresentableDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  SettingPresentableDelegate.swift
+//  
+//
+//  Created by JeongTaek Han on 2023/05/15.
+//
+
+import Foundation
+
+protocol SettingPresentableDelegate: AnyObject {
+    
+    func provideSettingViewController(of item: any SettingItemPresentable, presentAction: ((any SettingPage)?) -> Void)
+    
+    func action(for item: any SettingItemPresentable)
+    
+}
