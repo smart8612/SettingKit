8918a2554ba98c26b475930b75901ec2970b3ce9
JeongTaek Han <smart8612@gmail.com>
2023-05-15 17:49:49 +0900

    feat: define preview
    by using swiftui

--- Sources/SettingKit/SettingKit.swift
+++ Sources/SettingKit/SettingKit.swift
@@ -1,6 +1,34 @@
+import SwiftUI
+
+
 public struct SettingKit {
     public private(set) var text = "Hello, World!"
 
     public init() {
     }
+}
+
+struct SettingView: UIViewControllerRepresentable {
+    
+    typealias UIViewControllerType = UINavigationController
+    
+    private let mainPage = MainSettingPage()
+    
+    func makeUIViewController(context: Context) -> UIViewControllerType {
+        mainPage.viewControllerEmbeddedInNavigationController
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        context.coordinator
+    }
+    
+}
+
+
+struct SettingKit_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        SettingView()
+    }
+    
 }
