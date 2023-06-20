c46c777216e48831b4bd064f060edf277d17cbb2
JeongTaek Han <smart8612@gmail.com>
2023-06-20 16:02:03 +0900

    refactor: remove unused code #2

--- Tests/SettingKitTests/SettingKitTests.swift
+++ Tests/SettingKitTests/SettingKitTests.swift
@@ -6,6 +6,5 @@ final class SettingKitTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SettingKit().text, "Hello, World!")
     }
 }
