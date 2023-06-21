94642103b8c9e30c5603428e10c66eb047ce9e50
JeongTaek Han <smart8612@gmail.com>
2023-06-21 15:35:24 +0900

    docs: write documentation about the Settings page extension #2

--- Sources/SettingsKit/Extensions/Extensions+SettingsPage.swift
+++ Sources/SettingsKit/Extensions/Extensions+SettingsPage.swift
@@ -10,6 +10,13 @@ import UIKit
 
 public extension SettingsPage {
     
+    /// A view controller instance that represents the setting collection
+    /// view controller.
+    ///
+    /// A view controller use the view model which is declared
+    /// on the settings page.
+    /// If view model conforms ``SettingsCollectionViewControllerDelegate``
+    /// then set view controller's delegate object to view model.
     var viewController: UIViewController {
         let viewController = SettingsCollectionViewController(viewModel: viewModel)
         viewController.settingDelegate = viewModel as? any SettingsCollectionViewControllerDelegate
