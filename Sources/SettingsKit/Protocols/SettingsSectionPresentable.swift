66733d8b27c3573024f25d62f436ef7742492a17
JeongTaek Han <smart8612@gmail.com>
2023-06-21 14:59:18 +0900

    docs: write document about the SettingsSectionPresentable #2

--- Sources/SettingsKit/Protocols/SettingsSectionPresentable.swift
+++ Sources/SettingsKit/Protocols/SettingsSectionPresentable.swift
@@ -8,9 +8,20 @@
 import Foundation
 
 
+/// A type that declares a setting section.
+///
+/// You can declares a setting section that can be shown at the settings collection
+/// view.
 public protocol SettingsSectionPresentable: Hashable {
     
+    /// The title of the settings section.
+    ///
+    /// The title is shown at the collection view's header supplementary cell.
     var title: String? { get }
+    
+    /// The description of the settings section.
+    ///
+    /// The description is shown at the collection view's footer supplementary cell.
     var description: String? { get }
     
 }
