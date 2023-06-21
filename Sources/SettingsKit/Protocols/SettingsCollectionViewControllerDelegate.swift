e1587258b29ee8061ff06ef247f85a4ad7adbea3
JeongTaek Han <smart8612@gmail.com>
2023-06-21 15:14:39 +0900

    docs: write documentation about the settings collection view controller delegate #2

--- Sources/SettingsKit/Protocols/SettingsCollectionViewControllerDelegate.swift
+++ Sources/SettingsKit/Protocols/SettingsCollectionViewControllerDelegate.swift
@@ -8,8 +8,15 @@
 import Foundation
 
 
+/// The methods adopted by the object you use to manage user interactions with items in a settings collection view.
+///
+/// A settings collection view controller delegate manages user interactions with the setting items contents and performing actions on those items.
 public protocol SettingsCollectionViewControllerDelegate: AnyObject {
     
+    /// Provides the setting page to the settings collection view controller.
+    /// - Parameters:
+    ///   - item: The settings item which is chosen.
+    ///   - presentAction: The closure that can be used to present the settings page.
     func provideSettingPage(of item: any SettingsItemPresentable, presentAction: ((any SettingsPage)?) -> Void)
     
     func action(for item: any SettingsItemPresentable)
