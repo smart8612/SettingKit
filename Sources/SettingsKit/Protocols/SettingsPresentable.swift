35dae4ab1c43ed2d1dad6ce72e7c2f94c20f5332
JeongTaek Han <smart8612@gmail.com>
2023-06-21 14:51:54 +0900

    docs: write document about settings presentable protocol #2

--- Sources/SettingsKit/Protocols/SettingsPresentable.swift
+++ Sources/SettingsKit/Protocols/SettingsPresentable.swift
@@ -8,11 +8,18 @@
 import Foundation
 
 
+/// A type that can declares the setting options.
 public protocol SettingsPresentable {
     
+    /// An associated type that the section type must conform.
     associatedtype Section: SettingsSectionPresentable
+    
+    /// An associated type that the item type must conform.
     associatedtype Item: SettingsItemPresentable
     
+    /// The list of the setting item.
+    ///
+    /// You can declare setting options on ``items`` property.
     var items: [Item] { get }
     
 }
