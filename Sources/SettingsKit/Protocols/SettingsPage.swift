dd1e7afe60f740e8281365e2e5b4310ad8572c05
JeongTaek Han <smart8612@gmail.com>
2023-06-21 14:44:19 +0900

    docs: write documentation about settingspage protocol #2

--- Sources/SettingsKit/Protocols/SettingsPage.swift
+++ Sources/SettingsKit/Protocols/SettingsPage.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 
+/// A type that declares a settings page.
 public protocol SettingsPage {
     
+    /// An associated type that view model have to conform.
     associatedtype ViewModelType: SettingsPresentable
     
+    /// The title of the settings page which can be seen at the navigation bar.
     var title: String? { get }
+    
+    /// The view model of the settings page which can declare setting options.
     var viewModel: ViewModelType { get }
+    
+    /// The view controller of the settings page.
     var viewController: UIViewController { get }
     
 }
